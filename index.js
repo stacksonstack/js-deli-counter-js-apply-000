f7aa6678556ae18204a3c151bf4fed3bd6c35555
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-07-13 22:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function takeANumber(currentLine, name){
-  currentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  
+  
 }
