e1303127ea90426371a6944f2690c85149c034a4
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-07-13 22:55:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber(currentLine, name){
+  currentLine.push(name);
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
 
-function nowServing(katzDeliLine){
-  if(katzDeliLine.length === 0){
+function nowServing(currentLine){
+  if(currentLine.length === 0){
     return "There is nobody waiting to be served!"
   }
-  return katzDeliLine.shift()
-  
+  return currentLine.shift()
+}
+
+function currentLine(currentLine){
+  if(currentLine.length === 0){
+    return "The line is currently empty."
+  }
+  for(){
+    
+  }
 }
