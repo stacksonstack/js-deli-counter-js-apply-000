1f558b8161496c1b2017febf2d258e792eb9ff5a
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-07-13 23:14:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(currentLine){
   if(currentLine.length === 0){
     return "There is nobody waiting to be served!"
   }
-  return `Currently serving ${currentLine.shift()}`
+  return `Currently serving ${currentLine.shift()}.`
 }
 
 function currentLine(currentLine){
